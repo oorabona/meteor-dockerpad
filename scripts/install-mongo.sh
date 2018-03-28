943032e08b9a89631965995fadcb7b5d7408e136
Olivier ORABONA <oorabona@users.noreply.github.com>
2018-03-28 21:30:49 +0200

    Simplified mongodb installation

--- scripts/install-mongo.sh
+++ scripts/install-mongo.sh
@@ -2,11 +2,5 @@
 set -e
 source $BUILD_SCRIPTS_DIR/common.sh
 
-if [ -f $APP_SOURCE_DIR/launchpad.conf ]; then
-  source <(grep INSTALL_MONGO $APP_SOURCE_DIR/launchpad.conf)
-fi
-
-if [ "$INSTALL_MONGO" == "true" ]; then
-  p "Installing MongoDB..."
-  apt install -y mongodb
-fi
+p "Installing MongoDB..."
+apt install -y mongodb
