1f56a4e4c1bd0b6ac02fd772f755d7e45733b444
Olivier ORABONA <oorabona@users.noreply.github.com>
2018-03-28 21:32:09 +0200

    Meteor not anymore removed and instead used to call node/npm

--- scripts/post-build-cleanup.sh
+++ scripts/post-build-cleanup.sh
@@ -4,9 +4,6 @@ source $BUILD_SCRIPTS_DIR/common.sh
 
 p "Performing final cleanup..."
 
-# get out of the src dir, so we can delete it
-cd $APP_BUNDLE_DIR
-
 # Clean out docs
 rm -rf /usr/share/{doc,doc-base,man,locale,zoneinfo}
 
@@ -16,23 +13,23 @@ rm -rf /var/lib/{cache,log}
 # remove app source
 rm -rf $APP_SOURCE_DIR
 
-# remove meteor
-rm -rf /usr/local/bin/meteor
-rm -rf /root/.meteor
-
-# clean additional files created outside the source tree
-rm -rf /root/{.npm,.cache,.config,.cordova,.local}
-rm -rf /tmp/*
-
 # remove npm
-npm cache clean --force
+su-exec meteor meteor npm cache clean --force
 
 # make sure we keep only production code
-npm prune --production
+su-exec meteor meteor npm prune --production
+
+# remove meteor
+# rm -rf /usr/local/bin/meteor
+# rm -rf /home/meteor/.meteor
+
+# clean additional files created outside the source tree
+rm -rf ~meteor/{.npm,.cache,.config,.cordova,.local}
+rm -rf /tmp/*
 
 # remove os dependencies
-apt-get purge -y --auto-remove bzip2 ca-certificates curl git python
-apt-get -y autoremove
-apt-get -y clean
-apt-get -y autoclean
+apt purge -y --auto-remove bzip2 ca-certificates curl git python gcc
+apt -y autoremove
+apt -y clean
+apt -y autoclean
 rm -rf /var/lib/apt/lists/*
